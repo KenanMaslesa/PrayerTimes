8b3967656bf3067d25c6bbd9bb4a56d9f5f2472a
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-01-20 00:04:48 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -450,3 +450,21 @@ $('.print').on('click', function () {
   w.print();
   w.close();
 })
+
+
+function iOS() {
+  return [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+if (iOS()) {
+  $('.mapboxgl-ctrl-geocoder').hide();
+}
