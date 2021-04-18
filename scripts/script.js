4e4e1a34d6eb83d1c88c9fbbb4a76ab03db1e62e
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-04-18 14:21:11 +0200

    .

--- scripts/script.js
+++ scripts/script.js
@@ -591,7 +591,12 @@ function setTime(time) {
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 
-  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  if(flag){
+    timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`
+  }
+  else{
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  }
   dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
