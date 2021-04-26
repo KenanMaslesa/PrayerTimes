0e6497525094b11c02887b9cf4ebc4679311dace
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-04-26 23:48:09 +0200

    service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,6 @@
-const staticGlobalPrayerTimes = "pwa"
+
+const staticCacheName = "site-static"
+const dynamicCacheName = "site-dynamic"
 const assets = [
   "./",
   "./style/style.min.css",
@@ -25,6 +27,25 @@ const assets = [
   "./audio/beep.mp3",
   "./ionicons/css/ionicons.css",
   "./ionicons/css/ionicons.min.css",
+  "./ionicons/fonts/ionicons.eot",
+  "./ionicons/fonts/ionicons.svg",
+  "./ionicons/fonts/ionicons.ttf",
+  "./ionicons/fonts/ionicons.woff",
+  "https://api.mapbox.com/v4/mapbox.mapbox-traffic-v1/12/2259/1491.vector.pbf?sku=101nTCV0wjtvB&access_token=pk.eyJ1Ijoia2VuYW5tYXNsZXNhIiwiYSI6ImNranVrZmJ3YzIwdWgyeWwxdnc0N3c1eWEifQ.NU5jwn8auapNwbJVrjKTfg",
+  "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8,mapbox.mapbox-terrain-v2.json?secure&access_token=pk.eyJ1Ijoia2VuYW5tYXNsZXNhIiwiYSI6ImNranVrZmJ3YzIwdWgyeWwxdnc0N3c1eWEifQ.NU5jwn8auapNwbJVrjKTfg",
+  "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11?access_token=pk.eyJ1Ijoia2VuYW5tYXNsZXNhIiwiYSI6ImNranVrZmJ3YzIwdWgyeWwxdnc0N3c1eWEifQ.NU5jwn8auapNwbJVrjKTfg",
+  "https://unpkg.com/@mapbox/mapbox-sdk@0.12.1/umd/mapbox-sdk.min.js",
+  "https://api.mapbox.com/styles/v1/mapbox/streets-v11?access_token=pk.eyJ1Ijoia2VuYW5tYXNsZXNhIiwiYSI6ImNranVrZmJ3YzIwdWgyeWwxdnc0N3c1eWEifQ.NU5jwn8auapNwbJVrjKTfg",
+  "https://api.mapbox.com/styles/v1/mapbox/traffic-night-v2?access_token=pk.eyJ1Ijoia2VuYW5tYXNsZXNhIiwiYSI6ImNranVrZmJ3YzIwdWgyeWwxdnc0N3c1eWEifQ.NU5jwn8auapNwbJVrjKTfg",
+  "https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css",
+  "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.css",
+  "https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.js",
+  "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js",
+  "https://unpkg.com/@mapbox/mapbox-sdk/umd/mapbox-sdk.min.js",
+  "https://i.stack.imgur.com/qq8AE.gif",
+  "https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.2/howler.core.min.js",
+  "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+  "https://richtr.github.io/NoSleep.js/dist/NoSleep.min.js",
 ]
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
@@ -38,7 +59,12 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return response || fetch(event.request).then(fetchRes => {
+          return cashes.open(dynamicCacheName).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          })
+        });
       })
   )
-}) 
+})
