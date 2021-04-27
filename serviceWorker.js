fe08d0a2061ea4c451157f1dc0d4fb9bdb603909
KenanMaslesa <kenoo.maslesa@gmail.com>
2021-04-27 04:49:21 +0200

    .

--- serviceWorker.js
+++ serviceWorker.js
@@ -37,12 +37,7 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetch(event.request).then(fetchRes => {
-          return caches.open(dynamicCacheName).then(cache => {
-            cache.put(event.request.url, fetchRes.clone());
-            return fetchRes;
-          })
-        });
+        return response || fetch(event.request)
       })
   )
 })
